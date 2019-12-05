d1c88bc4668d87eb7992549d45055e7e73d9b1bb
S2LF <49229770+S2LF@users.noreply.github.com>
2019-12-05 14:01:49 +0100

    Add files via upload

---
+++ js/JQuery/script.js
@@ -0,0 +1,12 @@
+// function helloWorld(){
+//     alert('Hello World !');
+// }
+
+//  // affichera une alerte "Hello World Sainior !"
+// helloWorld();
+
+// function helloWorld(nom){
+//     alert('Hello World, ' + nom + '!');
+// }
+
+// helloWorld('Sainior'); // affichera une alerte "Hello World Sainior !"
