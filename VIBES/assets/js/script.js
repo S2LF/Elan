a11cfc400d604443311b1e4f683d824cd2ad516a
S2LF <49229770+S2LF@users.noreply.github.com>
2019-12-23 17:05:56 +0100

    Add files via upload

--- VIBES/assets/js/script.js
+++ VIBES/assets/js/script.js
@@ -1,5 +1,5 @@
 
-
+// Gestion des filtres Gallery
 $("#filters a").on("click", function(event){
     event.preventDefault()
     let filter = $(this).attr("id")
@@ -14,3 +14,41 @@ $("#filters a").on("click", function(event){
 
 })
 
+
+// Gestion du Details
+$("details").on('click', function(){
+        $("details").removeAttr("open")
+        $(this).attr("open")
+})
+
+// Gestion goutte
+$("div#goutteMain").on('click', function(){
+    $("div.goutte ul").toggleClass("display")
+})
+
+
+// Red
+$(".red").on('click', function(){
+    $(':root').css('--main-color', 'red')
+})
+// Blue
+$(".blue").on('click', function(){
+    $(':root').css('--main-color', 'blue')
+})
+// Purple
+$(".purple").on('click', function(){
+    $(':root').css('--main-color', 'purple')
+})
+
+// $('.red').toggle(function () {
+//     $("dl>dt>i").css('color', 'red');
+// }, function () {
+//     $("#user_button").css('');
+// });
+
+
+$("#burgerNav").on('click', function(){
+
+    $("nav").toggleClass('menu-mobile')
+
+})
